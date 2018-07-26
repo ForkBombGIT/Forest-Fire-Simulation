cb11898ce945785459f353a9c839df1936aea523
Iphiram <josh-gorman@hotmail.com>
2018-07-25 22:06:27 -0400

    Built the base of the project, half working implementation focused on accessing the canvas from react components

---
+++ components/Tiles/tree.js
@@ -0,0 +1,7 @@
+const Tree = (props) => (
+  <div style={layoutStyle}>
+    <canvas id="myCanvas" width="100%" height="100"></canvas>
+  </div>
+)
+
+export default Tree
--- pages/index.js
+++ pages/index.js
@@ -1,1 +1,11 @@
-export default () => <div>welcome to next.js!</div>		
+import Layout from '../components/Layout.js'
+import Canvas from '../components/Canvas.js'
+
+export default () => (
+    <Layout>
+       <p>This is the about page</p>
+       <Canvas>
+          <canvas id="myCanvas" width="100%" height="100"></canvas>
+       </Canvas>
+    </Layout>
+)
