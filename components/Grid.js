9063fdbc9f956548bee8af15bde879da980c6805
Iphiram <josh-gorman@hotmail.com>
2018-10-01 21:57:54 -0400

    Replaced bindings with arrow functions and data with a data object

--- components/Grid.js
+++ components/Grid.js
@@ -8,96 +8,95 @@ export default class Grid extends React.PureComponent {
 
     this.grid = []
     this.timerTick = 0;
-    this.dimensions = {}
-    this.unique = 0;
+    this.gridData = {
+    		canvasSize: {},
+        probTree: 0.99,
+        probBurn: 0.001,
 
-    //this.createGrid = this.createGrid.bind(this);
-    this.renderTile = this.renderTile.bind(this);
-    this.update = this.update.bind(this);
+    }
+    this.unique = 0;
   }
 
   componentWillUpdate(prevProps) {
-  	let width = this.props.canvRef.current.width
-  	  this.props.canvRef.current.width = this.props.canvRef.current.offsetWidth;
-   		this.props.canvRef.current.height = this.props.canvRef.current.offsetHeight;
+    let width = this.props.canvRef.current.width
+    this.props.canvRef.current.width = this.props.canvRef.current.offsetWidth;
+    this.props.canvRef.current.height = this.props.canvRef.current.offsetHeight;
 
-    if (this.props.canvRef.current.width !== this.dimensions.canvasSize[0] || this.props.canvRef.current.height !== this.dimensions.canvasSize[1]) {
-      this.dimensions.canvasSize = [this.props.canvRef.current.width, this.props.canvRef.current.height]
-   		this.createGrid()
+    if (this.props.canvRef.current.width !== this.gridData.canvasSize.x || this.props.canvRef.current.height !== this.gridData.canvasSize.y) {
+      this.gridData.canvasSize.x = this.props.canvRef.current.width;
+      this.gridData.canvasSize.y = this.props.canvRef.current.height;
+
+      this.createGrid()
     }
     this.update();
   }
 
   componentDidMount(prevProps) {
-  	this.props.canvRef.current.width = this.props.canvRef.current.offsetWidth;
+    this.props.canvRef.current.width = this.props.canvRef.current.offsetWidth;
     this.props.canvRef.current.height = this.props.canvRef.current.offsetHeight;
-    this.dimensions.canvasSize = [this.props.canvRef.current.width, this.props.canvRef.current.height]
+    this.gridData.canvasSize.x = this.props.canvRef.current.width;
+    this.gridData.canvasSize.y = this.props.canvRef.current.height;
 
-  	this.generate()
+    this.generate()
     this.forceUpdate()
   }
 
   createGrid = () => {
-    let propTree = 0.5;
-  	var prevArr = JSON.parse(JSON.stringify(this.grid));
+    var prevArr = JSON.parse(JSON.stringify(this.grid));
     var tempArr = [];
     this.grid = [];
-    let width = this.dimensions.canvasSize[0]
-    let height = this.dimensions.canvasSize[1]
-    let x = this.dimensions.numCol = ~~(width / 50);
-    let y = this.dimensions.numRow = ~~(height / 50);
+
+    let width = this.gridData.canvasSize.x
+    let height = this.gridData.canvasSize.y
+    let x = this.gridData.numCol = ~~(width / 50);
+    let y = this.gridData.numRow = ~~(height / 50);
 
     for (var w = 0; w < y; w++) {
       for (var h = 0; h < x; h++) {
-          if (prevArr[w][h] == undefined) {
-          	tempArr.push([(width/x) * h, 50 * w, width/x, Math.random() > propTree ? "tree" : "empty"])
-          }
-          else {
-          	console.log(width/x)
-          	tempArr.push([(width/x) * h, 50 * w, width/x, prevArr[w][h][3]]);
-          }
-
+        if (prevArr[w][h] == undefined) {
+          tempArr.push([(width / x) * h, 50 * w, width / x, Math.random() > this.gridData.probTree ? "tree" : "empty"])
+        } else {
+          console.log(width / x)
+          tempArr.push([(width / x) * h, 50 * w, width / x, prevArr[w][h][3]]);
+        }
       }
       this.grid.push(tempArr)
       tempArr = []
     }
   };
 
-  update() {
-  	let prevArray = JSON.parse(JSON.stringify(this.grid));
-    let probTree = 0.99;
-    let probBurn = 0.001;
+  update = () => {
+    let prevArray = JSON.parse(JSON.stringify(this.grid));
 
-  	for (var h = 0; h < this.grid.length; h++) {
-      for (var w = 0; w < this.grid[h].length; w++) {
-        if (prevArray[h][w][3] == "empty") Math.random() > probTree ? this.grid[h][w][3] ="tree" : this.grid[h][w][3] = "empty";
-        else if (prevArray[h][w][3] == "tree") {
-        	if (	(h > 0) && (prevArray[h-1][w][3] == "fire") ||
-          			(h < prevArray.length-1) && (prevArray[h+1][w][3] == "fire") ||
-              	(w > 0) && (prevArray[h][w-1][3] == "fire") ||
-          			(w  < prevArray[h].length-1) && (prevArray[h][w+1][3] == "fire")) {
-              	this.grid[h][w][3] = "fire"
-              }
-           	else {
-          		if (Math.random() < probBurn) this.grid[h][w][3] = "fire";
-          	}
+    for (var w = 0; w < this.grid.length; w++) {
+      for (var h = 0; h < this.grid[w].length; h++) {
+        if (prevArray[w][h][3] == "empty") {
+          Math.random() > this.gridData.probTree ? this.grid[w][h][3] = "tree" : this.grid[w][h][3] = "empty";
+        } else if (prevArray[w][h][3] == "tree") {
+          if ((w > 0) && (prevArray[w - 1][h][3] == "fire") ||
+            (w < prevArray.length - 1) && (prevArray[w + 1][h][3] == "fire") ||
+            (h > 0) && (prevArray[w][h - 1][3] == "fire") ||
+            (h < prevArray[w].length - 1) && (prevArray[w][h + 1][3] == "fire")) {
+            this.grid[w][h][3] = "fire"
+          } else {
+            if (Math.random() < this.gridData.probBurn) this.grid[w][h][3] = "fire";
           }
-          else if (prevArray[h][w][3] == "fire") this.grid[h][w][3] = "empty"
-        }
+        } else if (prevArray[w][h][3] == "fire") this.grid[w][h][3] = "empty"
       }
     }
+  }
 
   generate = () => {
-    let propTree = 0.5;
     var tempArr = [];
-    let width = this.dimensions.canvasSize[0]
-    let height = this.dimensions.canvasSize[1]
-    let x = this.dimensions.numCol = ~~(width / 50);
-    let y = this.dimensions.numRow = ~~(height / 50);
+
+    let width = this.gridData.canvasSize.x
+    let height = this.gridData.canvasSize.y
+    let x = this.gridData.numCol = ~~(width / 50);
+    let y = this.gridData.numRow = ~~(height / 50);
 
     for (var w = 0; w < y; w++) {
       for (var h = 0; h < x; h++) {
-          	tempArr.push([(width/x) * h, 50 * w, width/x, Math.random() > propTree ? "tree" : "empty"])
+        tempArr.push([(width / x) * h, 50 * w, width / x, Math.random() > this.gridData.probTree ? "tree" : "empty"])
       }
       this.grid.push(tempArr)
       tempArr = []
@@ -105,20 +104,19 @@ export default class Grid extends React.PureComponent {
     //this.grid = tempArr;
   };
 
-  renderTile(i) {
-  		this.unique++;
-      return <Tile key={this.unique} {...this.props} startPos={i}/>
+  renderTile = (i) => {
+    this.unique++;
+    return <Tile key = { this.unique } { ...this.props } startPos = { i } />
   }
 
   render() {
-  //console.log(this.grid)
-    return <div>
-      {
-        (this.grid).map ((n) => {
-       	  return(
-        		(n).map ((x) => {
-          		return this.renderTile(x)
-         	  })
+    //console.log(this.grid)
+    return <div> {
+        (this.grid).map((n) => {
+          return (
+            (n).map((x) => {
+              return this.renderTile(x)
+            })
           )
         })
       }
--- components/Tile.js
+++ components/Tile.js
@@ -2,42 +2,40 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 export default class Tile extends React.PureComponent {
-    constructor(props) {
-      super(props);
-      this.timerTick = 1;
-      this.stateColour = {
-      	"tree"  : "green",
-        "empty" : "white",
-        "fire"  : "red"
-      };
+  constructor(props) {
+    super(props);
+    this.timerTick = 1;
+    this.stateColour = {
+      "tree": "green",
+      "empty": "white",
+      "fire": "red"
+    };
+  }
 
-      this.drawRectangle = this.drawRectangle.bind(this);
+  drawRectangle = () => {
+    var ctx = this.props.canvRef.current.getContext("2d")
+    ctx.beginPath()
+
+    ctx.fillStyle = this.stateColour[this.props.startPos[3]];
+    ctx.rect(this.props.startPos[0], this.props.startPos[1], this.props.startPos[2], 50);
+    ctx.fill();
+    ctx.strokeStyle = this.stateColour[this.props.startPos[3]];
+    ctx.stroke();
+  }
+
+
+  componentDidMount() {
+    this.drawRectangle()
+  }
+
+  componentWillUpdate() {
+    if (this.props.currentTimer !== this.timerTick) {
+      this.timerTick = this.props.timer
+      this.drawRectangle()
     }
+  }
 
-    drawRectangle() {
-      var ctx = this.props.canvRef.current.getContext("2d")
-      ctx.beginPath()
-
-      ctx.fillStyle = this.stateColour[this.props.startPos[3]];
-      ctx.rect(this.props.startPos[0], this.props.startPos[1],this.props.startPos[2],50);
-      ctx.fill();
-      ctx.strokeStyle = this.stateColour[this.props.startPos[3]];
-      ctx.stroke();
-    }
-
-
-    componentDidMount() {
-			this.drawRectangle()
-    }
-
-    componentWillUpdate() {
-      if (this.props.currentTimer !== this.timerTick) {
-      	this.timerTick = this.props.timer
-				this.drawRectangle()
-  		}
-  	}
-
-  	render() {
-   	 	return (null);
-  	}
+  render() {
+    return (null);
+  }
 }
